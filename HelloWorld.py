1c7dce5610111925a38192cb1b5f7613ed728775
Artem Kisel <artemkisel@yahoo.com>
2013-02-22 11:27:52 -0500

    Hello World

--- HelloWorld.py
+++ HelloWorld.py
@@ -22,39 +22,9 @@ class ThreadAlphabetUrls(threading.Thread):
             self.alphabet_queue.task_done()
 
 
-
-def getSameInitialPlayersUrls(url):
-    players_urls = []
-    link = urllib.request.urlopen(url)
-    soup = BeautifulSoup(link.read())
-
-    if soup.find('div', {'style': 'padding: 6px; font-weight: bold;'}) is not None:
-        return players_urls
-
-    for l in soup.find_all('a'):
-        candidate = l.get('href')
-        candidate = str(candidate)
-        if ('http' not in candidate) and (candidate not in players_urls) and ('/ice/player.htm?id' in candidate):
-            players_urls.append('http://www.nhl.com' + candidate + '&view=splits')
-
-    node = soup.find('div', {'class': 'resultCount'}).next
-    results = node.split()
-    result_left_off = int(results[0].split('-')[1])
-    result_total = int(results[2])
-
-    if result_left_off < result_total:
-        next_page = url[:url.rfind('=') + 1] + str(result_left_off // 50 + 1)
-        return players_urls + getSameInitialPlayersUrls(next_page)
-    elif result_left_off == result_total:
-        return players_urls
-    else:
-        print('something went wrong')
-
-
 alphabet_urls = getAlphabetUrls()
 
 alphabet_queue = queue.Queue()
-
 
 for _ in range(len(alphabet_urls)):
     t1 = ThreadAlphabetUrls(alphabet_queue)
