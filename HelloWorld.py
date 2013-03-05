6e2430363cd6cd941c16b8dab4ae557743136b7c
Artem Kisel <artemkisel@yahoo.com>
2013-03-05 12:15:49 -0500

    up

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,36 +3,42 @@ import queue
 import threading
 import urllib.request
 from bs4 import BeautifulSoup
-from time import time
+# from time import time, sleep
 
 
-def getAlphabetUrls():
+def getABCUrls():
     pattern = 'http://www.nhl.com/ice/playersearch.htm?letter='
     return [(pattern + letter + '&pg=1') for letter in [char for char in string.ascii_uppercase]]
 
 
 class MyThread(threading.Thread):
-    def __init__(self, alphabet_urls_queue, players_urls):
-        self.alphabet_urls_queue = alphabet_urls_queue
-        self.players_urls = players_urls
-        self.lock = threading.RLock()
-
+    def __init__(self, abc_urls_queue, abc_queue_condition, players_queue, players_queue_condition):
         threading.Thread.__init__(self)
+        self.abc_urls_queue = abc_urls_queue
+        self.abc_queue_condition = abc_queue_condition
+        self.players_queue = players_queue
+        self.players_queue_condition = players_queue_condition
 
     def run(self):
-        while not self.alphabet_urls_queue.empty():
-            alphabet_url = self.alphabet_urls_queue.get()
-            sameInitialPlayersUrls = self.getSameInitialPlayersUrls(alphabet_url)
+        while True:
+            self.abc_queue_condition.acquire()
+            if not self.abc_urls_queue.empty():
+                abc_url = self.abc_urls_queue.get()
+                self.abc_urls_queue.task_done()
+                self.abc_queue_condition.notify_all()
+                self.abc_queue_condition.release()
+            else:
+                # print('empty')
+                self.abc_queue_condition.notify_all()
+                self.abc_queue_condition.release()
+                break
+            sameInitialPlayersUrls = self.getSameInitialPlayersUrls(abc_url)
+            print(self.getName(), abc_url, len(sameInitialPlayersUrls))
 
-            self.lock.acquire()
-            self.players_urls.extend(sameInitialPlayersUrls)
-            self.lock.release()
-
-            self.alphabet_urls_queue.task_done()
+            with self.players_queue_condition:
+                [self.players_queue.put(url) for url in sameInitialPlayersUrls]
 
     def getSameInitialPlayersUrls(self, alphabet_url):
-        # print('%s will work on %s here' % (self.getName(), alphabet_url))
-        # sleep(1)
         players_urls = []
 
         url = alphabet_url
@@ -62,23 +68,22 @@ class MyThread(threading.Thread):
             print('something went wrong')
 
 
-
 def main():
     print('main started')
-    for num_of_threads in range(2, 27):
-        num_of_simulations = 50
-        start = time()
-        for i in range(num_of_simulations):
-            alphabet_urls = getAlphabetUrls()
-            players_urls = []
 
-            alphabet_urls_queue = queue.Queue()
-            [alphabet_urls_queue.put(url) for url in alphabet_urls]
+    abc_urls = getABCUrls()
+    abc_urls_queue = queue.Queue()
+    [abc_urls_queue.put(url) for url in abc_urls]
+    abc_queue_condition = threading.Condition(threading.RLock())
 
-            threads = [MyThread(alphabet_urls_queue, players_urls) for _ in range(num_of_threads)]
-            [t.start() for t in threads]
-            [t.join() for t in threads]
-        total_time = time() - start
-        print('%d threads: %d simulations in %d sec. %d sec per simulation. list check: %d' % (num_of_threads, num_of_simulations, total_time, total_time / num_of_simulations, len(players_urls)))
+    players_queue = queue.Queue()
+    players_queue_condition = threading.Condition(threading.RLock())
+
+    threads = [MyThread(abc_urls_queue, abc_queue_condition, players_queue, players_queue_condition) for _ in range(5)]
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+
+    print(players_queue.qsize())
     print('main finished')
+
 main()
